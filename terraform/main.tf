acbad5725dd6c0698ed922c5ba87601cdf91b357
Benedicta Onyekwere <onyekwerebenedicta@gmail.com>
2023-03-17 08:24:12 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,97 @@
+resource "digitalocean_droplet" "server1" {
+  image      = "ubuntu-20-04-x64"
+  name       = "server1"
+  region     = "lon1"
+  size       = "s-1vcpu-1gb"
+  ssh_keys   = [
+    data.digitalocean_ssh_key.Exam-Project.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file("var.pvt_key") #("~/.ssh/id_rsa")
+    host        = self.ipv4_address
+  }
+
+
+}
+
+# Define the firewall rules
+resource "digitalocean_firewall" "server1" {
+  name = "server1-firewall"
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9099"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9093"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9100"
+    source_addresses = ["0.0.0.0/0"]
+  }
+  }
+
+resource "digitalocean_droplet" "server2" {
+  image      = "ubuntu-20-04-x64"
+  name       = "server2"
+  region     = "lon1"
+  size       = "s-1vcpu-1gb"
+  ssh_keys   = [
+    data.digitalocean_ssh_key.Exam-Project.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file("var.pvt_key") #("~/.ssh/id_rsa")
+    host        = self.ipv4_address
+  }
+}
+
+# Define the firewall rules
+resource "digitalocean_firewall" "server2" {
+  name = "server2-firewall"
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0"]
+  }
+ inbound_rule {
+    protocol         = "tcp"
+    port_range       = "5432"
+    source_addresses = ["0.0.0.0/0"]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9100"
+    source_addresses = ["0.0.0.0/0"]
+  }
+  
